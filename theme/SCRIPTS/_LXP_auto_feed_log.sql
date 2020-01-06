7feca582249082edffd88d9faad3580d3f46c010
Jonathan Borthwick <jborthwickog@gmail.com>
2020-01-05 21:23:35 -0800

    Create new log table for persisting log history
    For every time the process writes new stuff, log that activity here if the page has the writelog parameter

---
+++ theme/SCRIPTS/_LXP_auto_feed_log.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jan 05, 2020 at 10:20 PM
+-- Server version: 5.6.41-84.1
+-- PHP Version: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `spinnis0_WPLXP`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `_LXP_auto_feed_log`
+--
+
+CREATE TABLE `_LXP_auto_feed_log` (
+  `AUTO_FEED_ID` bigint(20) NOT NULL,
+  `LOG_TEXT` text CHARACTER SET utf8 COLLATE utf8_unicode_520_ci NOT NULL,
+  `DATE_CREATED` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `LAST_UPDATED` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
